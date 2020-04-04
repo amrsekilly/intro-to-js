57ca0e100ecda52055d5dc19c8c4cbaa7f90cda2
Amr Sekilly <amr@pelcro.com>
2020-04-04 21:12:43 +0200

    Nested loops

---
+++ main.js
@@ -0,0 +1,12 @@
+const names = [
+  ["Amr", 27],
+  ["Mohamed", 25],
+  ["Mostafa", 28],
+  ["Remon", 27],
+];
+
+for (let i = 0; i < names.length; i++) {
+  for (let j = 0; j < names[i].length; j++) {
+    console.log(names[i][j]);
+  }
+}
